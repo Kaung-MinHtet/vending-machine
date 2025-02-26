e2682339c30362df240cfadbebc2c2171ec95b50
Kaung-MinHtet <kaungminhtet2712@gmail.com>
2025-02-26 21:02:53 +0630

    Test: commit tests

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    protected $model = Product::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word(),
+            'price' => $this->faker->randomFloat(2, 0.5, 10),
+            'quantity_available' => $this->faker->numberBetween(0, 50),
+        ];
+    }
+}
---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use App\Models\Transaction;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Transaction>
+ */
+class TransactionFactory extends Factory
+{
+    protected $model = Transaction::class;
+
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'product_id' => Product::factory(),
+            'quantity' => $this->faker->numberBetween(1, 5),
+            'total_price' => fn (array $attributes) => Product::find($attributes['product_id'])->price * $attributes['quantity'],
+        ];
+    }
+}
